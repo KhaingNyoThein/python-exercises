e90d463056d91760e1ef878151fa5ce5ec7b0584
KhaingNyoThein <khaingnyothein357@gmail.com>
2018-06-05 21:19:29 -0700

    More variables and printing

--- ex5.py
+++ ex5.py
@@ -5,11 +5,12 @@ my_weight=180#lbs
 my_eyes='Blue'
 my_teeth='White'
 my_hair='Brown'
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
-print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." %(my_eyes,my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print (f"Let's talk about {my_name}.")
+print (f"He's {my_height} inches tall.")
+print (f"He's {my_weight} pounds heavy.")
+print (f"Actually that's not too heavy.")
+print (f"He's got {my_eyes} eyes and {my_hair} hair.")
+print (f"His teeth are usually {my_teeth} depending on the coffee.") 
 #this line is trickly,try to get it exactly  right
-print "If I add %d, %d and %d  I get %d." % (my_age, my_height, my_weight, my_age+ my_height+ my_weight)
+total=my_age+my_height+my_weight
+print (f"If I add {my_age}, {my_height} and {my_weight}  I get {total}.")
