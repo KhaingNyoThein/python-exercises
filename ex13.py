c34c0acdd3f1978f48442532a245aabdc5cf6f23
KhaingNyoThein <khaingnyothein357@gmail.com>
2018-06-05 22:46:58 -0700

    parameter passing

--- ex13.py
+++ ex13.py
@@ -4,4 +4,4 @@ script, first, second, third= argv
 print("The script is called:",script)
 print("Your first variable is:",first)
 print("Your second variable is:",second)
-pritn("Your third variable is :",third)
+print("Your third variable is :",third)
