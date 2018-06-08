d7f814f5541e60f17913a5753ec81c482acdb820
KhaingNyoThein <khaingnyothein357@gmail.com>
2018-06-08 10:11:00 -0700

    Functions and files

--- ex20.py
+++ ex20.py
@@ -8,9 +8,9 @@ def rewind(f):
     f.seek(0)
 
 def print_a_line(line_count,f):
-    print(line_count,f.readLine())
+    print(line_count,f.readline())
 
-    current_file=open(input_file)
+    current_file = open(input_file)
 
     print("First let's print the whole file:\n")
     print_all(current_file)
@@ -18,7 +18,7 @@ def print_a_line(line_count,f):
     rewind(current_file)
 
     print ("Let's print three lines:")
-    current_line1 = 1
+    current_line = 1
     print_a_line(current_line, current_file)
 
     current_line = current_line + 1
