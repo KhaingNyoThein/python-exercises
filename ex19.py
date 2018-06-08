ca242dbccefe996e3e2a4075b26558c34801bf0b
KhaingNyoThein <khaingnyothein357@gmail.com>
2018-06-07 19:35:43 -0700

    Functions and variables

--- ex19.py
+++ ex19.py
@@ -10,11 +10,10 @@ cheese_and_crackers(20,30)
 print("We can just give use variables from our script:")
 amount_of_cheese= 10
 amount_of_crackers= 50
-
 cheese_and_crackers(amount_of_cheese,amount_of_crackers)
 
 print("We can even do math inside too:")
 cheese_and_crackers(10+20,5+6)
 
 print("And we can combine the two,variables and math:")
-cheese_and_crackers(amount_of_cheese+ 100,amount_of_cracke)
+cheese_and_crackers(amount_of_cheese+ 100,amount_of_crackers)
