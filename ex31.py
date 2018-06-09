1b0e319adb439b8865865737779acce8333778a9
KhaingNyoThein <khaingnyothein357@gmail.com>
2018-06-08 20:35:11 -0700

    Making decision

--- ex31.py
+++ ex31.py
@@ -1,7 +1,7 @@
 print("""You enter a dark room with two doors.
          Do you go through door #1 or door #2?""")
 
-door = input( ">")
+door = input( "> ")
 
 if door == "1":
    print("There's a giant bear here eating a cheese cake.")
@@ -16,6 +16,7 @@ if bear == "1":
 
 elif bear == "2":
     print("The bear eats your legs off. Good job!")
+
 else:
     print(f"Well, doing {bear} is probably better.")
     print("Bear runs away.")
@@ -28,11 +29,11 @@ elif door == "2":
      
      insanity = input("> ")
      
-     if insanity == "1" or insanity == "2":
+if insanity == "1" or insanity == "2":
      print("Your body survives powered by a mind of jello.")
      print("Good job!")
      
-     else:
+else:
      print("The insanity rots your eyes into a pool of muck.")
      print("Good job!")
 
