d30b4906ab5a9d8334c676deea52a2a7a20d1bbc
KhaingNyoThein <khaingnyothein357@gmail.com>
2018-06-08 20:41:21 -0700

    Making decision

--- ex31.py
+++ ex31.py
@@ -3,7 +3,7 @@ print("""You enter a dark room with two doors.
 
 door = input( "> ")
 
-if door == "1":
+ if door == "1":
    print("There's a giant bear here eating a cheese cake.")
    print("What do you do?")
    print("1. Take the cake.")
@@ -11,17 +11,17 @@ if door == "1":
 
    bear = input("> ")
 
-if bear == "1":
+ if bear == "1":
    print("The bear eats your face off. Good job!")
 
-elif bear == "2":
+ elif bear == "2":
     print("The bear eats your legs off. Good job!")
 
-else:
+ else:
     print(f"Well, doing {bear} is probably better.")
     print("Bear runs away.")
-
-elif door == "2":
+ 
+ elif door == "2":
      print("You stare into the endless abyss at Cthulhu's retina.")
      print("1. Blueberries.")
      print("2. Yellow jacket clothespins.")
@@ -29,13 +29,13 @@ elif door == "2":
      
      insanity = input("> ")
      
-if insanity == "1" or insanity == "2":
-     print("Your body survives powered by a mind of jello.")
-     print("Good job!")
+     if insanity == "1" or insanity == "2":
+         print("Your body survives powered by a mind of jello.")
+         print("Good job!")
      
-else:
-     print("The insanity rots your eyes into a pool of muck.")
-     print("Good job!")
+     else:
+         print("The insanity rots your eyes into a pool of muck.")
+         print("Good job!")
 
-else:
+ else:
      print("You stumble around and fall on a knife and die. Good!")
