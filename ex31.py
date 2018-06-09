e9ad5b4e0b9d23c1bbed77fa414526a0d66e02f8
KhaingNyoThein <khaingnyothein357@gmail.com>
2018-06-08 22:04:50 -0700

    Making decision

--- ex31.py
+++ ex31.py
@@ -11,16 +11,16 @@ if door == "1":
 
    bear = input("> ")
 
-if bear == "1":
-   print("The bear eats your face off. Good job!")
+   if bear == "1":
+    print("The bear eats your face off. Good job!")
 
-elif bear == "2":
+   elif bear == "2":
     print("The bear eats your legs off. Good job!")
 
-else:
+   else:
     print(f"Well, doing {bear} is probably better.")
     print("Bear runs away.")
- 
+
 elif door == "2":
      print("You stare into the endless abyss at Cthulhu's retina.")
      print("1. Blueberries.")
@@ -28,10 +28,11 @@ elif door == "2":
      print("3. Understanding revolvers yelling melodies.")
      
      insanity = input("> ")
-if insanity == "1" or insanity == "2":
+     
+     if insanity == "1" or insanity == "2":
          print("Your body survives powered by a mind of jello.")
          print("Good job!")
-else:
+     else:
          print("The insanity rots your eyes into a pool of muck.")
          print("Good job!")
 
